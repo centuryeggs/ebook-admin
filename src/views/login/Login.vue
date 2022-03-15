c162e4e714269fa3c35551369937d7847e24b80b
centuryeggs <junjianch@gmail.com>
2022-03-15 23:59:47 +0800

    feat:login

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="login">login</div>
+  <div class="login">
+    <div class="left">
+      <img src="./../../assets/images/login-books.png" alt="" />
+    </div>
+    <div class="right">
+      <div class="login-title">登陆</div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,3 +16,16 @@ export default defineComponent({
   data() {},
 })
 </script>
+
+<style lang="sass" scoped>
+.login
+  height: 100%
+  width: 100%
+  background-color: #2b3144
+  display: flex
+  justify-content: space-between
+  align-items: center
+  .left
+    img
+      width: 250px
+</style>
