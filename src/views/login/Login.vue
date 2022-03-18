451d1329fa3e83deb330a61511f5f219c34305d9
陈俊坚 <junjianch@gmail.com>
2022-03-18 16:47:16 +0800

    feat:登陆页

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,19 +1,17 @@
 <template>
-  <div class="flex bg-[#2b3144] h-full">
-    <div class="items-center justify-center hidden w-full h-full basis-1/2 xl:flex xl:flex-col">
-      <img class="w-60" src="./../../assets/images/login-books.png" alt="" />
-      <h1 class="text-3xl font-bold text-white">电子书后台管理系统</h1>
-    </div>
-    <div class="items-center justify-center w-full h-full basis-1/2 xl:flex xl:flex-col">
-      <h1 class="text-3xl font-bold text-white">登陆</h1>
+  <div class="w-full h-full px-20 pt-4 pb-20 bg-[#2b3144]">
+    <div class="flex items-center justify-around w-full h-full">
+      <div class="flex-col items-center justify-center hidden h-full xl:flex">
+        <img class="w-60" src="./../../assets/images/login-books.png" />
+        <h1 class="text-3xl font-bold text-white">电子书后台管理系统</h1>
+      </div>
+      <div class="flex flex-col items-center justify-center h-full">
+        <LoginForm />
+      </div>
     </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-export default defineComponent({
-  name: 'Login',
-  data() {},
-})
+<script lang="ts" setup>
+  import LoginForm from './LoginForm.vue'
 </script>
---
+++ src/views/login/LoginForm.vue
@@ -0,0 +1,60 @@
+<template>
+  <h1 class="pb-2 text-3xl font-bold text-center text-white">登录</h1>
+  <a-form
+    class="w-80"
+    :model="formState"
+    name="basic"
+    autocomplete="off"
+    @finish="onFinish"
+    @finishFailed="onFinishFailed"
+  >
+    <a-form-item
+      name="username"
+      :rules="[{ required: true, message: '请输入您的用户名!' }]"
+    >
+      <a-input v-model:value="formState.username" size="large" />
+    </a-form-item>
+
+    <a-form-item
+      name="password"
+      :rules="[{ required: true, message: '请输入您的密码!' }]"
+    >
+      <a-input-password v-model:value="formState.password" size="large" />
+    </a-form-item>
+
+    <a-form-item>
+      <a-button
+        type="primary"
+        html-type="submit"
+        size="large"
+        shape="round"
+        block
+        >登录</a-button
+      >
+    </a-form-item>
+  </a-form>
+</template>
+
+<script setup lang="ts">
+  import { reactive } from 'vue'
+
+  interface FormState {
+    username: string
+    password: string
+  }
+  const formState = reactive<FormState>({
+    username: 'admin',
+    password: 'admin',
+  })
+
+  const onFinish = (values: any) => {
+    console.log('Success:', values)
+    handleLogin()
+  }
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo)
+  }
+  async function handleLogin() {
+    alert('登录')
+  }
+</script>
