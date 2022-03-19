d74e00092374dc336d92247d6a159c1cbd624b5c
centuryeggs <junjianch@gmail.com>
2022-03-19 23:07:50 +0800

    modify:登陆校验

--- src/views/login/LoginForm.vue
+++ src/views/login/LoginForm.vue
@@ -4,21 +4,16 @@
     class="w-80"
     :model="formState"
     name="basic"
+    :rules="rules"
     autocomplete="off"
     @finish="onFinish"
     @finishFailed="onFinishFailed"
   >
-    <a-form-item
-      name="username"
-      :rules="[{ required: true, message: '请输入您的用户名!' }]"
-    >
+    <a-form-item name="username">
       <a-input v-model:value="formState.username" size="large" />
     </a-form-item>
 
-    <a-form-item
-      name="password"
-      :rules="[{ required: true, message: '请输入您的密码!' }]"
-    >
+    <a-form-item name="password">
       <a-input-password v-model:value="formState.password" size="large" />
     </a-form-item>
 
@@ -29,6 +24,7 @@
         size="large"
         shape="round"
         block
+        :loading="loading"
         >登录</a-button
       >
     </a-form-item>
@@ -36,7 +32,8 @@
 </template>
 
 <script setup lang="ts">
-  import { reactive } from 'vue'
+  import { reactive, ref } from 'vue'
+  import type { RuleObject } from 'ant-design-vue/lib/form'
 
   interface FormState {
     username: string
@@ -44,9 +41,22 @@
   }
   const formState = reactive<FormState>({
     username: 'admin',
-    password: 'admin',
+    password: 'adminadmin',
   })
-
+  const loading = ref(false)
+  let validatePass = async (_rule: RuleObject, value: string) => {
+    if (value === '') {
+      return Promise.reject('请输入密码!')
+    } else if (value.length < 8) {
+      return Promise.reject('密码不可少于8位!')
+    } else {
+      return Promise.resolve()
+    }
+  }
+  const rules = {
+    username: [{ required: true, message: '请输入您的用户名!' }],
+    password: [{ required: true, validator: validatePass, trigger: 'change' }],
+  }
   const onFinish = (values: any) => {
     console.log('Success:', values)
     handleLogin()
@@ -55,6 +65,6 @@
     console.log('Failed:', errorInfo)
   }
   async function handleLogin() {
-    alert('登录')
+    loading.value = true
   }
 </script>
